ec2e4c881f3112136ed31325aa3e6cebfa4baad8
Daniela Houck <42559335+DanielaHouck@users.noreply.github.com>
2023-05-15 21:06:51 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,70 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import TelaPrincipal from "./src/pages/TelaPrincipal";
+import Tela1 from "./src/pages/Tela1.jsx";
+import Tela2 from "./src/pages/Tela2.jsx";
+import Tela3 from "./src/pages/Tela3.jsx";
+import Tela4 from "./src/pages/Tela4.jsx";
+import Tela5 from "./src/pages/Tela5.jsx";
+import Tela6 from "./src/pages/Tela6.jsx";
+import Tela7 from "./src/pages/Tela7.jsx";
+
+
+const { Screen, Navigator } = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Navigator initialRouteName="TelaPrincipal" screenOptions={{headerShown:false}}>
+          <Screen
+            name="TelaPrincipal"
+            component={TelaPrincipal}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Screen
+            name="Tela1"
+            component={Tela1}
+          />
+          <Screen
+            name="Tela2"
+            component={Tela2}
+          />
+          <Screen
+            name="Tela3"
+            component={Tela3}
+          />
+          <Screen
+            name="Tela4"
+            component={Tela4}
+          />
+          <Screen
+            name="Tela5"
+            component={Tela5}
+          />
+          <Screen
+            name="Tela6"
+            component={Tela6}
+          />
+          <Screen
+            name="Tela7"
+            component={Tela7}
+          />
+        </Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: "10%",
+  },
+});
